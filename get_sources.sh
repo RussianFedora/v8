93a6d3b80b93c7d54b5ba1f0c0238164c74dd92f
Tigro <ashejn@gmail.com>
2012-03-28 18:30:39 +0400

    update to 3.9.24.1

--- get_sources.sh
+++ get_sources.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -sO https://api.opensuse.org/public/source/network:chromium/v8/v8.3.9.19.0.tar.lzma
+curl -sO https://api.opensuse.org/public/source/network:chromium/v8/v8.3.9.24.1.tar.lzma
